10b25f55f4b0818a0c37a3b0649ec17a539b2c4f
arobin87 <alejandra.robinson87@gmail.com>
2024-01-26 22:41:02 -0500

    Hover behavior and media query

---
+++ js/index.js
@@ -0,0 +1,24 @@
+console.log("Your index.js file is loaded correctly!");
+
+var x = window.matchMedia("max-width: 600px")
+
+$(".searchButton").hover(function(){
+    $(".searchButton").toggleClass("active");
+    if ($(".searchButton").hasClass("active")) {
+      if(window.innerWidth < 600){
+        console.log("small screen");
+        $(this).find(".searchForm").css("height", "23.5rem");
+      }
+      else{
+        $(this).find(".searchForm").css("height", "27.5rem");
+        console.log("big screen");
+      }
+      $(this).find(".workDescription").css("opacity","1");
+      $(this).find(".workButton").css("opacity","1");
+    }
+    else {
+      $(this).find(".searchForm").css('height', "2.18rem");
+      $(this).find(".workDescription").css("opacity","0");
+      $(this).find(".workButton").css("opacity","0");
+    }
+  });
