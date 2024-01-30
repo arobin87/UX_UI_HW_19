e3be0ed58831b7b71fed7f0b1a031a6d1c5d1e78
arobin87 <alejandra.robinson87@gmail.com>
2024-01-30 18:18:27 -0500

    Changed name of workBackground and workHover

--- js/index.js
+++ js/index.js
@@ -2,22 +2,20 @@ console.log("Your index.js file is loaded correctly!");
 
 var x = window.matchMedia("max-width: 600px")
 
-$(".searchButton").hover(function(){
-    $(".searchButton").toggleClass("active");
-    if ($(".searchButton").hasClass("active")) {
+$(".workHover").hover(function(){
+    $(".workHover").toggleClass("active");
+    if ($(".workHover").hasClass("active")) {
       if(window.innerWidth < 600){
-        console.log("small screen");
-        $(this).find(".searchForm").css("height", "23.5rem");
+        $(this).find(".workBackground").css("height", "23.5rem");
       }
       else{
-        $(this).find(".searchForm").css("height", "35.5rem");
-        console.log("big screen");
+        $(this).find(".workBackground").css("height", "35.5rem");
       }
       $(this).find(".workDescription").css("opacity","1");
       $(this).find(".workButton").css("opacity","1");
     }
     else {
-      $(this).find(".searchForm").css('height', "2.18rem");
+      $(this).find(".workBackground").css('height', "2.18rem");
       $(this).find(".workDescription").css("opacity","0");
       $(this).find(".workButton").css("opacity","0");
     }
